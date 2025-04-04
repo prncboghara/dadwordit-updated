faa60909bd63af0bf1c24517635908f78ab974ab
Prince Boghara <prncboghara@gmail.com>
2025-04-04 16:30:29 +0530

    updated blog from hubspot to strapi

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const axios = require('axios');
 const moment = require('moment');
+const showdown = require('showdown');
 
 const getRecentBlogs = async () => {
     try {
-        const HUBSPOT_API_URL = "https://api.hubapi.com/cms/v3/blogs/posts";
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+        const TOKEN = process.env.STRAPI_TOKEN;
+        const STRAPI_URL = process.env.STRAPI_URL;
+        const API_PATH = `${STRAPI_URL}/api/articles?populate[author]=true&populate[main_image]=true&populate[category]=true&fields=title,slug,publishedAt,documentId&sort=publishedAt:desc&pagination[page]=1&pagination[pageSize]=3`;
 
-        const response = await axios.get(HUBSPOT_API_URL, {
-            headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` },
-            params: {
-                limit: 3, // Fetch the latest 3 blogs
-                offset: 0, // Optional, set to 0 for the first page of results
-                state: 'PUBLISHED'
-            }
+        const response = await axios.get(API_PATH, {
+            headers: { Authorization: `Bearer ${TOKEN}` }
         });
 
-        return response.data.results.map(blog => ({
+        return response.data.data.map(blog => ({
             id: blog.id,
-            name: blog.name,
-            created: moment(blog.created).format('MMM DD, YYYY'),
-            featuredImage: blog.featuredImage,
-            url: blog.slug
+            name: blog.title,
+            created: moment(blog.publishedAt).format('MMM DD, YYYY'),
+            featuredImage: `${STRAPI_URL}${blog.main_image.url}`,
+            url: `/blog/${blog.slug}`
         }));
 
     } catch (error) {
@@ -29,71 +26,74 @@ const getRecentBlogs = async () => {
     }
 };
 
-const getTags = async () => {
-    try {
-        const HUBSPOT_API_URL = "https://api.hubapi.com/cms/v3/blogs/tags";
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+// const getTags = async () => {
+//     try {
+//         const HUBSPOT_API_URL = "https://api.hubapi.com/cms/v3/blogs/tags";
+//         const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
 
-        const response = await axios.get(HUBSPOT_API_URL, { headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` } });
-        return response.data.results;
-    } catch (error) {
-        console.log('error', error)
-        return []
-    }
-}
+//         const response = await axios.get(HUBSPOT_API_URL, { headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` } });
+//         return response.data.results;
+//     } catch (error) {
+//         console.log('error', error)
+//         return []
+//     }
+// }
 
 const getBlogs = async () => {
     try {
-        const tags = await getTags()
+        
         // get blogs
-        const HUBSPOT_API_URL = "https://api.hubapi.com/cms/v3/blogs/posts";
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+        const TOKEN = process.env.STRAPI_TOKEN;
+        const STRAPI_URL = process.env.STRAPI_URL;
+        const API_PATH = `${STRAPI_URL}/api/articles?populate[author]=true&populate[cover]=true&populate[category]=true&populate[main_image]=true&fields=title,slug,publishedAt,documentId&sort=publishedAt:desc`;
 
-        const response = await axios.get(HUBSPOT_API_URL, {
-            headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` },
+        const response = await axios.get(API_PATH, {
+            headers: { Authorization: `Bearer ${TOKEN}`, 'Content-Type': 'application/json' },
         });
 
-        return response.data.results.map(blog => ({
+        return response.data.data.map(blog => ({
             id: blog.id,
-            name: blog.name,
-            created: moment(blog.created).format('MMM DD, YYYY'),
-            featuredImage: blog.featuredImage,
-            authorName: blog.authorName,
-            metaDescription: blog.metaDescription,
-            tags: blog.tagIds.map(tagId => {
-                return tags.find(tag => tag.id === tagId.toString()); // Ensure ID is in string format
-            }),
-            slug: blog.slug
+            name: blog.title,
+            created: moment(blog.publishedAt).format('MMM DD, YYYY'),
+            featuredImage: `${STRAPI_URL}${blog.main_image.url}`,
+            authorName: blog?.author?.name,
+            // metaDescription: blog.metaDescription,
+            // tags: blog.tagIds.map(tagId => {
+            //     return tags.find(tag => tag.id === tagId.toString()); // Ensure ID is in string format
+            // }),
+            slug: `/blog/${blog.slug}`
         }));
     } catch (error) {
-        console.log('error', error)
         return []
     }
 };
 
 const getBlog = async (slug) => {
     try {
-        const HUBSPOT_API_URL = `https://api.hubapi.com/cms/v3/blogs/posts?slug=${slug}`;
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+        const TOKEN = process.env.STRAPI_TOKEN;
+        const STRAPI_URL = process.env.STRAPI_URL;
+        const API_PATH = `${STRAPI_URL}/api/articles?filters[slug][$eq]=${slug}&populate=*`;
 
-        const response = await axios.get(HUBSPOT_API_URL, {
-            headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` },
+        const response = await axios.get(API_PATH, {
+            headers: { Authorization: `Bearer ${TOKEN}` },
         });
 
-        const comments = await getComments(response.data.results[0].id)
-
-        return response.data.results.map(blog => ({
+        let converter = new showdown.Converter();
+        converter.setOption('tables', true);
+        return response.data.data.map(blog => ({
             id: blog.id,
-            name: blog.name,
-            authorName: blog.authorName,
-            state: blog.state,
-            created: moment(blog.created).format('MMM DD, YYYY'),
-            featuredImage: blog.featuredImage,
+            name: blog.title,
+            authorName: blog.author.name,
+            created: moment(blog.publishedAt).format('MMM DD, YYYY'),
+            main_image: `${STRAPI_URL}${blog.main_image.url}`,
+            cover: `${STRAPI_URL}${blog.cover.url}`,
             slug: blog.slug,
-            postBody: blog.postBody,
-            metaDescription: blog.metaDescription,
-            comments: comments,
-            blogUrl: `https://dadwordit.com/${blog.slug}`
+            content: converter.makeHtml(blog.content),
+            comments: [],
+            blogUrl: `https://dadwordit.com/blog/${blog.slug}`,
+            seo: blog.blocks.find(b => b.__component === 'shared.seo')
+            // state: blog.state,
+            // metaDescription: blog.metaDescription,
         }));
 
     } catch (error) {
@@ -102,54 +102,54 @@ const getBlog = async (slug) => {
     }
 };
 
-const getComments = async (id) => {
-    try {
-        const HUBSPOT_API_URL = `https://api.hubapi.com/comments/v3/comments/?contentId=${id}`;
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+// const getComments = async (id) => {
+//     try {
+//         const HUBSPOT_API_URL = `https://api.hubapi.com/comments/v3/comments/?contentId=${id}`;
+//         const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
 
-        const response = await axios.get(HUBSPOT_API_URL, {
-            headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` },
-        });
+//         const response = await axios.get(HUBSPOT_API_URL, {
+//             headers: { Authorization: `Bearer ${HUBSPOT_API_KEY}` },
+//         });
 
-        return response.data.objects.map(c => ({
-            createdAt: moment(c.createdAt).format('MMM DD, YYYY'),
-            userName: c.userName,
-            userEmail: c.userEmail,
-            comment: c.comment
-        }));
+//         return response.data.objects.map(c => ({
+//             createdAt: moment(c.createdAt).format('MMM DD, YYYY'),
+//             userName: c.userName,
+//             userEmail: c.userEmail,
+//             comment: c.comment
+//         }));
 
-    } catch (error) {
-        console.log("error", error)
-        return []
-    }
-};
+//     } catch (error) {
+//         console.log("error", error)
+//         return []
+//     }
+// };
 
-const postComment = async (req, res) => {
-    const { id, title, userEmail, userName, comment } = req.body;
+// const postComment = async (req, res) => {
+//     const { id, title, userEmail, userName, comment } = req.body;
 
-    try {
-        const response = await axios.post('https://api.hubapi.com/comments/v3/comments', {
-            contentId: id,
-            collectionId: id,
-            contentTitle: title,
-            userEmail: userEmail,
-            userName: userName,
-            comment: comment
-        }, {
-            headers: {
-                Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
-                'Content-Type': 'application/json',
-            },
-        });
-        response.data.createdAt = moment(response.data.createdAt).format('MMM DD, YYYY'),
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error('Error adding comment:', error.response?.data || error.message);
-        res.status(500).json({ error: 'Failed to add comment' });
-    }
+//     try {
+//         const response = await axios.post('https://api.hubapi.com/comments/v3/comments', {
+//             contentId: id,
+//             collectionId: id,
+//             contentTitle: title,
+//             userEmail: userEmail,
+//             userName: userName,
+//             comment: comment
+//         }, {
+//             headers: {
+//                 Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
+//                 'Content-Type': 'application/json',
+//             },
+//         });
+//         response.data.createdAt = moment(response.data.createdAt).format('MMM DD, YYYY'),
+//         res.status(200).json(response.data);
+//     } catch (error) {
+//         console.error('Error adding comment:', error.response?.data || error.message);
+//         res.status(500).json({ error: 'Failed to add comment' });
+//     }
 
-};
+// };
 
 
 
-module.exports = { getRecentBlogs, getBlogs, getTags, getBlog, postComment }
+module.exports = { getRecentBlogs, getBlogs, getBlog }
---
+++ routes/public.js
@@ -0,0 +1,150 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const SEO_CONFIG = require('../seo/config')
+
+const { getRecentBlogs, getBlogs, getBlog, postComment } = require('../controllers/blog')
+
+const portfolioConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/portfolio-config.json'), 'utf8'));
+const testimonialConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/testimonial-config.json'), 'utf8'));
+const clientConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/client-config.json'), 'utf8'));
+
+router.get('/', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('index', { 
+        recent_blogs: recent_blogs,
+        portfolioItems: portfolioConfig.portfolioItems,
+        testimonials: testimonialConfig.testimonials,
+        clients: clientConfig.clients,
+        ...SEO_CONFIG.index,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/about', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('about', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.about,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/service', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('service', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.service,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/our-work', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('our-work', { 
+        recent_blogs: recent_blogs,
+        portfolioItems: portfolioConfig.portfolioItems,
+        ...SEO_CONFIG.our_work,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/blog', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    const blogs = await getBlogs()
+    res.render('blog', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.blog,
+        blogs: blogs,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/blog/:slug', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    const blog = await getBlog(`${req.params.slug}`);
+    if(blog) {
+        const _blog = blog[0]
+        res.render('blog-single', { 
+            recent_blogs: recent_blogs,
+            blog: _blog,
+            trackingId: process.env.G_TRACKING_ID,
+            title: `${_blog.seo.metaTitle} | Dadword IT`,
+            meta: {
+                description: _blog.seo.metaDescription,
+                author: _blog.authorName
+            },
+            og: {
+                title: _blog.seo.metaTitle,
+                description: _blog.seo.metaDescription,
+                image: _blog.main_image,
+                url: `https://dadwordit.com/blog/${req.params.slug}`,
+                type: 'website'
+            },
+            twitter: null,
+            schema: null,
+            canonical: null
+        });
+    } else {
+        res.render('blog-single', { })
+    }
+});
+
+router.get('/career', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('career', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.career,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/lets-talk', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('lets-talk', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.lets_talk,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/contact-us', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('contact-us', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.contact_us,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/faq', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('faq', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.customer_faq,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/privacy-policy', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('privacy-policy', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.privacy_policy,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+router.get('/terms-and-conditions', async (req, res) => {
+    let recent_blogs = await getRecentBlogs()
+    res.render('terms-and-conditions', { 
+        recent_blogs: recent_blogs,
+        ...SEO_CONFIG.terms_and_condition,
+        trackingId: process.env.G_TRACKING_ID
+    });
+});
+
+// router.post('/add-comment', postComment);
+
+module.exports = router;
